724eb91fe48987d9b78cb449e2434e69459a58f6
benone <benone@gmail.com>
2024-11-21 14:15:46 +0100

    database file

---
+++ database.sql
@@ -0,0 +1,10 @@
+create database ista;
+use ista;
+
+create table stagiares(matStagiare int primary key,
+nomStagiare varchar(150),
+prenomStagiare varchar(150),
+filiereStagiare varchar(150),
+anneeEtude date,
+typeBac varchar(150),
+anneeBac date);
